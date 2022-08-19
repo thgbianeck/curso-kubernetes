6f1d44e1dad6559fb1b1960462e838e27d2c3a99
Thiago Moreira Bianeck <thgbianeck@gmail.com>
2022-08-19 00:46:01 -0300

    Creando msvc cursos

---
+++ msvc-cursos/src/test/java/br/com/bieniek/springcloud/msvc/cursos/MsvcCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.springcloud.msvc.cursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
